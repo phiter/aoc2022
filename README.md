4cc6027386f0e8eb67b7edfc74edc0f604d86236
Phiter Fernandes <phiterfernandes@gmail.com>
2023-12-02 17:34:23 -0300

    add day3 solution

--- README.md
+++ README.md
@@ -13,7 +13,7 @@
 
 [![Day](https://badgen.net/badge/01/%E2%98%85%E2%98%85/green)](src/day01)
 [![Day](https://badgen.net/badge/02/%E2%98%85%E2%98%85/green)](src/day02)
-![Day](https://badgen.net/badge/03/%E2%98%86%E2%98%86/gray)
+[![Day](https://badgen.net/badge/03/%E2%98%85%E2%98%85/green)](src/day03)
 ![Day](https://badgen.net/badge/04/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/05/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/06/%E2%98%86%E2%98%86/gray)
@@ -76,16 +76,16 @@ Both parts: 0.797ms
 
 ```
 Day 02
-Time part 1: 1.972ms
-Time part 2: 2.309ms
-Both parts: 4.281ms
+Time part 1: 1.91ms
+Time part 2: 2.272ms
+Both parts: 4.182ms
 ```
 
 ```
 Day 03
-Time part 1: -
-Time part 2: -
-Both parts: -
+Time part 1: 0.486ms
+Time part 2: 0.79ms
+Both parts: 1.276ms
 ```
 
 ```
@@ -243,8 +243,8 @@ Both parts: -
 ```
 
 ```
-Total stars: 4/50
-Total time: 5.077ms
+Total stars: 6/50
+Total time: 6.255ms
 ```
 
 <!--/RESULTS-->
---
+++ src/day03/index.ts
@@ -0,0 +1,79 @@
+import run from "aocrunner";
+
+const parseInput = (rawInput: string) => rawInput.split('\n').map(l => l.trim());
+
+const atoz = [...Array(26).keys()].map(k => String.fromCharCode(k + 97));
+const AtoZ = [...Array(26).keys()].map(k => String.fromCharCode(k + 65));
+
+const itemPriorities: Record<string, number> = [...atoz, ...AtoZ].reduce((o, char, index) => ({
+  ...o,
+  [char]: index + 1
+}), {});
+
+const part1 = (rawInput: string) => {
+  const input = parseInput(rawInput);
+  let totalSum = 0;
+  for (const line of input) {
+    const firstHalf = line.slice(0, line.length / 2).split('');
+    const secondHalf = line.slice(line.length / 2, line.length);
+    const itemsInCommon = [...new Set(firstHalf.filter(i => secondHalf.includes(i)))];
+
+    totalSum += [...itemsInCommon].reduce((t, i) => t + itemPriorities[i], 0);
+  }
+  return totalSum;
+};
+
+const splitIntoChunks = (lines: string[], chunkSize: number) => {
+  const chunks = [];
+  for (let i = 0; i < lines.length; i += chunkSize) {
+    chunks.push(lines.slice(i, i + chunkSize));
+  }
+  return chunks;
+}
+
+const ELF_GROUP_SIZE = 3;
+
+const part2 = (rawInput: string) => {
+  const input = parseInput(rawInput);
+  const lineGroups = splitIntoChunks(input, ELF_GROUP_SIZE);
+  let totalSum = 0;
+  for (const group of lineGroups) {
+    const [first, second, third] = group;
+    const itemInCommon = [...new Set(first.split('').filter(item => second.includes(item) && third.includes(item)))]?.[0];
+    totalSum += itemPriorities[itemInCommon];
+  }
+  return totalSum;
+};
+
+run({
+  part1: {
+    tests: [
+      {
+        input: `vJrwpWtwJgWrhcsFMMfFFhFp
+                jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
+                PmmdzqPrVvPwwTWBwg
+                wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
+                ttgJtRGJQctTZtZT
+                CrZsJsPPZsGzwwsLwLmpwMDw`,
+        expected: 157,
+      },
+    ],
+    solution: part1,
+  },
+  part2: {
+    tests: [
+      {
+        input: `vJrwpWtwJgWrhcsFMMfFFhFp
+                jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
+                PmmdzqPrVvPwwTWBwg
+                wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
+                ttgJtRGJQctTZtZT
+                CrZsJsPPZsGzwwsLwLmpwMDw`,
+        expected: 70,
+      },
+    ],
+    solution: part2,
+  },
+  trimTestInputs: true,
+  onlyTests: false
+});
