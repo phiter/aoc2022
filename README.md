c67ca06edefd9678b6e48defdc8eb9a4e54d3320
Phiter Fernandes <phiterfernandes@gmail.com>
2023-12-02 20:04:16 -0300

    add day 4 solution

--- README.md
+++ README.md
@@ -14,7 +14,7 @@
 [![Day](https://badgen.net/badge/01/%E2%98%85%E2%98%85/green)](src/day01)
 [![Day](https://badgen.net/badge/02/%E2%98%85%E2%98%85/green)](src/day02)
 [![Day](https://badgen.net/badge/03/%E2%98%85%E2%98%85/green)](src/day03)
-![Day](https://badgen.net/badge/04/%E2%98%86%E2%98%86/gray)
+[![Day](https://badgen.net/badge/04/%E2%98%85%E2%98%85/green)](src/day04)
 ![Day](https://badgen.net/badge/05/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/06/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/07/%E2%98%86%E2%98%86/gray)
@@ -76,9 +76,9 @@ Both parts: 0.797ms
 
 ```
 Day 02
-Time part 1: 1.91ms
-Time part 2: 2.272ms
-Both parts: 4.182ms
+Time part 1: 1.998ms
+Time part 2: 2.304ms
+Both parts: 4.302ms
 ```
 
 ```
@@ -90,9 +90,9 @@ Both parts: 1.276ms
 
 ```
 Day 04
-Time part 1: -
-Time part 2: -
-Both parts: -
+Time part 1: 1.174ms
+Time part 2: 84.29ms
+Both parts: 85.464ms
 ```
 
 ```
@@ -243,8 +243,8 @@ Both parts: -
 ```
 
 ```
-Total stars: 6/50
-Total time: 6.255ms
+Total stars: 8/50
+Total time: 91.839ms
 ```
 
 <!--/RESULTS-->
---
+++ src/day04/index.ts
@@ -0,0 +1,80 @@
+import run from "aocrunner";
+
+const parseInput = (rawInput: string) => rawInput.split('\n');
+
+const parseLine = (line: string) => {
+  const { a1, a2, b1, b2 } = line.match('(?<a1>[0-9]+)-(?<a2>[0-9]+),(?<b1>[0-9]+)-(?<b2>[0-9]+)')?.groups!;
+  return {
+    a1: Number(a1),
+    a2: Number(a2),
+    b1: Number(b1),
+    b2: Number(b2)
+  };
+}
+const checkIsFullyContained = ({ a1, a2, b1, b2 }: ReturnType<typeof parseLine>) => {
+  return (a1 >= b1 && a2 <= b2) || (b1 >= a1 && b2 <= a2);
+}
+const part1 = (rawInput: string) => {
+  const input = parseInput(rawInput);
+  let fullyContainedLines = 0;
+  for (const line of input) {
+    const lineResult = parseLine(line);
+    const isFullyContained = checkIsFullyContained(lineResult);
+    if (isFullyContained) {
+      fullyContainedLines++;
+    }
+  }
+
+  return fullyContainedLines;
+};
+
+const checkHasOverlap = ({ a1, a2, b1, b2 }: ReturnType<typeof parseLine>) => {
+  return a1 >= b1 && a1 <= b2 || b1 >= a1 && b1 <= a2;
+}
+const part2 = (rawInput: string) => {
+  const input = parseInput(rawInput);
+  let overlappingLines = 0;
+  for (const line of input) {
+    const lineResult = parseLine(line);
+    const hasOverlap = checkHasOverlap(lineResult);
+    if (hasOverlap) {
+      overlappingLines++;
+    }
+  }
+
+  return overlappingLines;
+};
+
+run({
+  part1: {
+    tests: [
+      {
+        input: `2-4,6-8
+        2-3,4-5
+        5-7,7-9
+        2-8,3-7
+        6-6,4-6
+        2-6,4-8
+        6-6,6-6`,
+        expected: 3,
+      },
+    ],
+    solution: part1,
+  },
+  part2: {
+    tests: [
+      {
+        input: `2-4,6-8
+        2-3,4-5
+        5-7,7-9
+        2-8,3-7
+        6-6,4-6
+        2-6,4-8`,
+        expected: 4,
+      },
+    ],
+    solution: part2,
+  },
+  trimTestInputs: true,
+  onlyTests: false,
+});
